383c50f3873accca3d92ef701abfd7bfe61ccc0b
kukovisuals <clubhousenet@live.com>
2022-01-11 22:09:06 -0500

    created a simple game in react

---
+++ src/components/board/board.jsx
@@ -0,0 +1,11 @@
+import Square from '../square/square';
+
+const Board = ({ squares, onClick }) => (
+  <div className="board">
+    {squares.map((square, i) => (
+      <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
+  </div>
+);
+
+export default Board;
